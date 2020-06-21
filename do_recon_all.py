8dcf04de4f76be3c3973062b131991a23d68ac20
rukrei <Rudi.Kreidenhuber@gmail.com>
2020-06-21 23:51:52 +0200

    Automatic recon-all in successive mode now works...

--- do_recon_all.py
+++ do_recon_all.py
@@ -1,18 +1,42 @@
-import nipype
-from nipype.interfaces.freesurfer import ReconAll
-import nipype.interfaces.io as nio
 import nipype.pipeline.engine as pe
+from nipype.interfaces.freesurfer import ReconAll
 import os
+import glob
 
-do_recon_folder = '/Users/idrael/Playground/FS_Pipeline_Test'
-#do_recon_folder = os.environ['DO_RECON_FOLDER']
+do_recon_folder = os.environ['DO_RECON_FOLDER']
 #subjects_dir = os.environ[SUBJECTS_DIR]
+openmp = int(os.environ['FS_OPENMP'])
+
+# Overwrite env-variable input?
+#do_recon_folder = '/Users/idrael/Playground/FS_Pipeline_Test'
 subjects_dir = os.path.join(do_recon_folder, 'derivatives/anat')
-#openmp = int(os.environ[OPENMP])
-openmp = 1
-#n_jobs = int(os.environ['N_JOBS'])
+
 
 subjects = os.listdir(do_recon_folder)
+for s in subjects:
+    if '.DS_Store' in s or 'derivat' in s:
+        subjects.remove(s)
+
+def append_dcm_dir(s):
+    subjects_base = os.path.join(do_recon_folder, s)
+    try:
+        counter = 1
+        for file in glob.glob((subjects_base + '/1*/**/1000*'), recursive=True):
+            do_recon_dict[s] = file
+            counter += 1
+            if counter > 4:
+                return file
+                break
+    except OSError as e:
+        print(e)
+
+do_recon_dict = dict()
+for s in subjects:
+    do_recon_dict[s] = []
+    do_recon_dict[s] = append_dcm_dir(s)
+for s in subjects:
+    print(do_recon_dict[s])
+
 print('#'*50)
 print('#'*50)
 print('#'*50)
@@ -23,37 +47,17 @@ print('#'*50)
 print('#'*50)
 print('#'*50)
 
-
-recon_pipe = pe.Workflow(name='ReconAll_Pipeline')
-recon_pipe.base_dir = do_recon_folder
-
-
-#Datagrabber
-data = pe.MapNode(
-            interface = nio.DataGrabber(infields=['subject_id'], outfields=['struct']),
-            name='data', iterfield=['subject_id'])   
-data.inputs.base_directory = do_recon_folder
-data.iterables = [('subject_id', subjects)]
-data.inputs.template = '*'
-data.inputs.template_args = dict(struct = [['subject_id', 'struct']])
-data.inputs.subject_id = subjects
-data.inputs.sort_filelist = True
-results = data.run()
-
-data.run().outputs
-
 #ReconAll
-reconall = pe.MapNode(
-    interface = ReconAll(), name='recon_all',
-    iterfield=['subject_id', 'T1_files'])
-reconall.inputs.subject_id = subjects
 if not os.path.exists(subjects_dir):
     os.mkdir(subjects_dir)
-reconall.inputs.directive = all
-reconall.inputs.subjects_dir = subjects_dir
-reconall.inputs.openmp = 5
-
-recon_pipe.connect(data, 'struct', reconall, 'T1_files')
-
-
-#recon_pipe.run()
+for key in do_recon_dict:
+    if do_recon_dict[key] == None:
+        print(f'ReconAll for SUBJECT {key} FAILED - no dicom file found')
+    else:
+        reconall = ReconAll()
+        reconall.inputs.subject_id = key
+        reconall.inputs.T1_files = do_recon_dict[key]
+        reconall.inputs.directive = 'all'
+        reconall.inputs.subjects_dir = subjects_dir
+        reconall.inputs.openmp = openmp
+        reconall.run()
--- do_recon_all.sh
+++ do_recon_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export DO_RECON_FOLDER=$1
-export N_JOBS=5
-export OPENMP=5
+export N_JOBS=2
+export FS_OPENMP=2
 
 
 python -m do_recon_all.py
