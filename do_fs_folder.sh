49a47d714ff2ccb081671185f055a876df12adc1
Rudi Kreidenhuber <Rudi.Kreidenhuber@gmail.com>
2021-03-20 23:44:19 +0100

    Freesufer + hippo subfield segmentation pipeline

---
+++ do_fs_folder.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# usage = ./do_fs_folder.sh -f PATH/TO/FOLDER -sd PATH/TO/SUBJECTS_DIR -proc N_PROC
+# -f flag is mandatory
+# -sd flag is optional, if not set it will default to /PATH/TO/FOLDER/freesurfered
+# -proc flag is optional, if not set it will default to all available CPUs
+# 
+# --> treats every direct subdirectory-name as a new subject
+# --> runs recon-all + hippocampal subfield segmentation on every subject
+
+
+# Pipeline start
+# parse arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --folder )
+            shift
+            FOLDER=$1
+        ;;
+        -sd | --subjects_dir )
+            shift
+            SUBJ_DIR=$1
+        ;;
+        -s | --subjects )
+            shift
+            SUBJECT=$1
+        ;;              
+        -h | --help )
+            exit
+        ;;
+        -p | --proc )
+            shift
+            N_PROC=$1
+        ;; 
+        * )
+            exit 1
+        ;;
+    esac
+    shift
+done
+
+# count subject names
+i=1
+for d in $FOLDER/*
+do
+    dirs[i++]="${d%/}"
+done
+
+echo ""
+echo "######################################"
+echo "Processing folder --> $FOLDER"
+echo "######################################"
+echo ""
+
+NUM_SUBJECTS=${#dirs[@]}
+echo "- There are $NUM_SUBJECTS subjects in $FOLDER"
+
+# test, if a subject was set
+if [ -z ${SUBJECT+x} ]
+then
+    # set to all subjects, since variable wasn't set
+    SUBJECT="all"
+    echo "- SUBJECT was set to 'all', since none was specified"
+fi
+
+# test, if subjects_dir was set, otherwise set default
+if [ -z ${SUBJ_DIR+x} ]
+then
+    FS=freesurfered
+    SUBJ_DIR=$FOLDER$FS
+    echo "- SUBJECTS_DIR was set to $SUBJ_DIR, as no other directory was specified."
+fi
+
+# test, if proc flag was set and give info
+if [ -z ${N_PROC+x} ]
+then
+    N_PROC=999
+    echo "- Calculations will be performed with all CPUs available, as no limitation was specified."
+else
+    echo "- The execution will use $N_PROC parallel processes"
+fi
+
+# some info for the user...
+echo "- Nifti files will be stored in subjects subfolders, as they get funny names :-)"
+echo "- Freesurfer-files go to SUBJECTS_DIR."
+echo "- A python3 environment with nipype and dicom2nifti is necessary to run this program"
+echo " "
+echo " "
+echo "This will take some time...)"
+echo "----------------------------"
+echo " "
+
+#  Finally, execute python command
+python /home/rudi/git/beast_scripts/do_recon_all_nipype.py -f $FOLDER -sd $SUBJ_DIR -p $N_PROC -s $SUBJECT
--- do_recon_all_nipype.py
+++ do_recon_all_nipype.py
@@ -1,59 +1,119 @@
-import nipype
+#!/usr/bin/python
+
+import glob
+#import nipype
 from nipype.interfaces.freesurfer import ReconAll
-import nipype.interfaces.io as nio
-import nipype.pipeline.engine as pe
+from dicom2nifti import convert_directory
 import os
-
-#do_recon_folder = '/media/idrael/DATA/MEG/4freesurfer'
-do_recon_folder = os.environ['DO_RECON_FOLDER']
-subjects_dir = os.environ['SUBJECTS_DIR']
-#subjects_dir = os.path.join(do_recon_folder, 'derivatives/anat')
-openmp = int(os.environ['FS_OPENMP'])
-#openmp = 16
-#n_jobs = int(os.environ['N_JOBS'])
-
-subjects = os.listdir(do_recon_folder)
-print('#'*50)
-print('#'*50)
-print('#'*50)
-print('#'*50)
-print(f'The following subjects are supposed to be processed: {subjects}')
-print('#'*50)
-print('#'*50)
-print('#'*50)
-print('#'*50)
+import argparse
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing as mp
+import subprocess
 
 
-recon_pipe = pe.Workflow(name='ReconAll_Pipeline')
-recon_pipe.base_dir = do_recon_folder
+# grab parsed arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--folder', type=str)
+parser.add_argument('-s', '--subject', type=str)
+parser.add_argument('-sd', '--subjects_dir', type=str)
+parser.add_argument('-p', '--proc', type=int)
+
+args = parser.parse_args()
+
+if args.proc == 999:
+    n_jobs=mp.cpu_count()
+else:
+    n_jobs=args.proc
+
+print(" ")
+print("######################################")
+print(f"Now running do_recon_all_nipype.py with {n_jobs} streams")
+print(f" - there are {mp.cpu_count()} processors available on your computer.")
+print("######################################")
+print(" ")
+
+subject = args.subject
+subjects_dir = args.subjects_dir
+
+if subjects_dir == None:
+    subjects_dir = os.path.join(args.folder, "freesurfered")
+
+# get a list of available subjects
+subject_list = os.listdir(args.folder)
+
+#make sure desired subject exists, if one was specified
+if subject != "all":
+    assert (subject in subject_list), f"Your desired subject ({subject}) does not exist in {args.folder}."
+    subject_list = [subject]
+    
+if subject == "all":
+    print(f"\n\n\nBaseline folder contents are:\n{subject_list}\n--> those will be treated as Subjects")
+    
+
+# convert Dicom-folder to nifti-file
+def convert_dcm_folder(subject=None):
+    try:
+        anafolder = os.path.join(args.folder, subject)
+        folder = str(glob.glob((anafolder + '/1*/100*/100*'), recursive=True)[0])
+        convert_directory(folder, anafolder, compression=True, reorient=True)
+        return f"{subject} - DICOM folder converted to .nii.gz"
+    except Exception as e:
+        print(e)
 
 
-#Datagrabber
-data = pe.MapNode(
-            interface = nio.DataGrabber(infields=['subject_id'], outfields=['struct']),
-            name='data', iterfield=['subject_id'])   
-data.inputs.base_directory = do_recon_folder
-data.iterables = [('subject_id', subjects)]
-data.inputs.template = '*'
-data.inputs.template_args = dict(struct = [['subject_id', 'struct']])
-data.inputs.subject_id = subjects
-data.inputs.sort_filelist = True
-results = data.run()
-
-data.run().outputs
-
-#ReconAll
-reconall = pe.MapNode(
-    interface = ReconAll(), name='recon_all',
-    iterfield=['subject_id', 'T1_files'])
-reconall.inputs.subject_id = subjects
-if not os.path.exists(subjects_dir):
-    os.mkdir(subjects_dir)
-reconall.inputs.directive = all
-reconall.inputs.subjects_dir = subjects_dir
-reconall.inputs.openmp = 5
-
-recon_pipe.connect(data, 'struct', reconall, 'T1_files')
+# start a process for every subject
+with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+    res = executor.map(convert_dcm_folder, subject_list)
+    for r in res:
+        print(r)
 
 
-recon_pipe.run()
+# run recon-all + hippocampal subfield segmentation on nifti-file
+# helpers
+def run_recon_all(subject=None, subjects_dir=subjects_dir, openmp=1):
+    reconall = ReconAll()
+    if not os.path.isdir(subjects_dir):
+        print(f"Subjects directory {subjects_dir} does not exist, creating it.")
+        os.mkdir(subjects_dir)
+    if not os.path.isdir(os.path.join(subjects_dir, subject, "mri")):    
+        anafolder = os.path.join(args.folder, subject)
+        # catch error, if no .nii exists
+        try:
+            nii_file = glob.glob(anafolder + "/*.nii*")[0]
+            reconall.inputs.subject_id = subject
+            reconall.inputs.T1_files = nii_file
+            reconall.inputs.directive = 'all'
+            reconall.inputs.subjects_dir = subjects_dir
+            reconall.inputs.openmp = openmp
+            reconall.inputs.flags = "-3T"
+            return f"Now running recon-all for subject {subject}."
+            reconall.run()
+        except Exception as e:
+            print(e)
+    else:
+        print(f"A freesurfer segmentation of subject {subject} already exists in {subjects_dir} - aborting")
+
+with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+    res = executor.map(run_recon_all, subject_list)
+    for r in res:
+        print(r)
+
+# Hippocampal subfield segmentation
+# helper
+def run_shell_command(command):
+    subprocess.run(command, shell=True, capture_output=True, check=True)
+
+def do_hippo_seg(subject, subjects_dir=subjects_dir):
+    hippofile = os.path.join(subjects_dir, subject, "mri", "lh.hippoSfVolumes-T1.v21.txt")
+    if not os.path.isfile(hippofile):
+        print(f"Now running hippocampal segmentation for subject: {subject}")
+        hipposeg = f"segmentHA_T1.sh {subject} {subjects_dir}"
+        run_shell_command(hipposeg)
+        return f"{subject} - Hippocampal subfield segmentation completed"  
+    else:
+        print(f"Omitting hippocampal segmentation for subject {subj}, as it already exists")
+
+with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+    res = executor.map(do_hippo_seg, subject_list)
+    for r in res:
+        print(r)
